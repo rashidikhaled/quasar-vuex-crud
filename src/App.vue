56cee1428aa9cbd0c7bce67dc1c9152691c090c9
k7aled <khd.rashidi@gmail.com>
2025-04-26 18:24:11 +0330

    create toolbar and help section in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
-<template lang="">
-  <div>quasar-vuex-crud</div>
+<template>
+  <q-layout view="hHr LpR lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>Quasar CRUD with vuex</q-toolbar-title>
+        <q-space />
+        <q-btn flat round icon="help" @click="showHelp = true" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container> </q-page-container>
+
+    <q-dialog v-model="showHelp">
+      <q-card class="my-card">
+        <q-card-section>
+          <div class="text-h6">Help</div>
+        </q-card-section>
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </q-layout>
 </template>
 <script>
-export default {};
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      showHelp: false,
+    };
+  },
+};
 </script>
 <style lang=""></style>
