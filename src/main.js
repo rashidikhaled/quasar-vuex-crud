5067613301433ec78ca3cda0c401c75356305322
k7aled <khd.rashidi@gmail.com>
2025-04-26 22:38:50 +0330

    add store to main.js

--- src/main.js
+++ src/main.js
@@ -12,9 +12,10 @@ import "quasar/src/css/index.sass";
 // Assumes your root component is App.vue
 // and placed in same folder as main.js
 import App from "./App.vue";
+import { store } from "./store";
 
 const myApp = createApp(App);
-
+myApp.use(store);
 myApp.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
 });
