78dc989589d82b3138c6f3bbd641987c44249cf1
k7aled <khd.rashidi@gmail.com>
2025-04-28 00:57:35 +0330

    Xreate  Delete Confirmation and  Add/Edit Dialog dialog

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -38,12 +38,90 @@
       :columns="columns"
       :loading="loading"
       row-key="name">
+      <!--Custom for header section-->
+      <template v-slot:header-selection="scope">
+        <q-checkbox v-model="scope.selected" />
+      </template>
+      <!-- Actions column -->
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn
+            flat
+            round
+            color="primary"
+            title="Edit"
+            icon="edit"
+            @click="showAddDialog = true" />
+          <q-btn
+            flat
+            round
+            color="negative"
+            title="Delete"
+            icon="delete"
+            @click="showAddDialog = true" />
+        </q-td>
+      </template>
+      <!-- Loading state -->
       <template v-slot:loading>
         <q-inner-loading showing color="primary">
           <q-spinner-gears size="50px" color="primary" />
         </q-inner-loading>
       </template>
+      <!-- <q-dialog v-model="confirm" persistent>
+        <q-card>
+          <q-card-section class="row items-center">
+            <div>ADD-EDit</div>
+          </q-card-section>
+          <q-card-actions align="right"> </q-card-actions>
+        </q-card>
+      </q-dialog> -->
     </q-table>
+    <!-- Add/Edit Dialog -->
+    <q-dialog v-model="showAddDialog" persistent>
+      <q-card style="min-width: 400px">
+        <q-card-section class="row items-center">
+          <div class="text-h6">Edit - Delete</div>
+        </q-card-section>
+        <q-card-actions class="q-pt-none">
+          <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+            <div>
+              <q-input
+                label="Reset"
+                lazy-rules
+                :rules="[
+                  (val) => !!val || 'Title is required',
+                  (val) =>
+                    val.length <= 10 || 'Title must be less than 100 chars',
+                ]" />
+              <q-select
+                v-model="model"
+                :options="options"
+                label="User"
+                :rules="[(val) => !!val || 'User is required']" />
+              <q-toggle label="Complete" />
+            </div>
+          </q-form>
+        </q-card-actions>
+        <q-card-actions align="right">
+          <q-btn flat round color="primary" label="Cancel" v-close-popup />
+          <q-btn label="Save" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <!-- Delete Confirmation -->
+    <q-dialog v-model="showDeleteConfirm">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Confirm Delete</div>
+          <div>Are you sure you want to delete this item?</div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn label="Delete" color="negative" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 <script>
@@ -68,7 +146,9 @@ export default {
         },
         { name: "actions", label: "Actions", align: "right" },
       ],
-      rows: [],
+      slected: [],
+      showAddDialog: false,
+      showDeleteConfirm: false,
     };
   },
   computed: {
@@ -84,4 +164,4 @@ export default {
   },
 };
 </script>
-<style lang=""></style>
+<style scope></style>
